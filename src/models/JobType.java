eb3202971a4d8941b7bdcfd023fde02a4b80dd48
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-04-16 09:31:40 +0200

    add JobType enum

---
+++ src/models/JobType.java
@@ -0,0 +1,28 @@
+package src.models;
+
+public enum JobType {
+   
+    
+    FULL_TIME(120),
+    PART_TIME(80),
+    WORKING_STUDENT(80),
+    INTERNSHIP(120);    
+
+    private int workingHours;
+    
+    JobType(int workingHours){
+        this.workingHours = workingHours;
+    }
+
+    public void setWorkingHours(int workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public int getWorkingHours(){
+        return this.workingHours;
+    }
+
+
+
+    
+}
