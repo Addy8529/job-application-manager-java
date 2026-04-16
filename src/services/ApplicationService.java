dd4401fab7e918a82b7b75f4c02513a566df53ea
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-04-16 18:24:38 +0200

    Implemented AddCompany, AddApplication and findCompanyById

--- src/services/ApplicationService.java
+++ src/services/ApplicationService.java
@@ -1,42 +1,65 @@
 package src.services;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import src.models.Company;
 import src.models.JobApplication;
+import src.models.JobType;
 
 public class ApplicationService {
     
     private ArrayList<Company> companies;
     private ArrayList<JobApplication> applications;
-    
+    private int nextCompanyId;
+    private int nextApplicationId;
+
     public ApplicationService(){
         this.companies = new ArrayList<>();
         this.applications = new ArrayList<>();
+        this.nextCompanyId = 0;
+        this.nextApplicationId = 0;
     }
 
-    public void addCompany(Company company){
-
-        if(this.companies.contains(company)){
-            throw new IllegalArgumentException("Company already exists");
-        }else{
-            this.companies.add(company); 
-        }
+    public Company addCompany(String name, String description, String url, int numberOfEmployees ){
+        Company result= null;
         
+        if( name == null || name.isBlank() ){
+            throw new IllegalArgumentException("Company name must not be null or blank.");
+        }else{
+            for(Company company: companies){
+                if(company.getName().equalsIgnoreCase(name.trim())){
+                    throw new IllegalArgumentException("Company name already exists.");
+                }
+            }
+            result = new Company(nextCompanyId, name.trim(), description, url, numberOfEmployees);
+            nextCompanyId++;
+            companies.add(result);
+        }
+        return result;
     }
 
-    public void addApplication(JobApplication app){
-        if(this.applications.contains(app)){
-            System.out.println("Application already exists in the database.");
-        }else{
-            this.applications.add(app);
-        }
+    public JobApplication addApplication(int companyId, String roleTitle, JobType type, LocalDate dateApplied, LocalDate followUpDate, String notes ){
+        
+            if ( this.findCompanyById(companyId).equals(null)){
+                throw new IllegalArgumentException("No company associated with the given company id.");
+            }else{
+                String normalizedRoleTitel = roleTitle.trim();
+                if( normalizedRoleTitel == null || normalizedRoleTitel.isBlank() ){
+                    throw new IllegalArgumentException("Role Title cannot be null or blank.");
+                }else{
+                    JobApplication app = new JobApplication(nextApplicationId, companyId, roleTitle, type, dateApplied, followUpDate,notes);
+                    nextApplicationId++;
+                    this.applications.add(app);
+                    return app;
+                }
+            }
         
     }
 
     public ArrayList<Company> getCompanies(){
-        return this.companies;
+        return new ArrayList<>(this.companies);
     }
 
     public ArrayList<JobApplication> getApplications(){
@@ -54,6 +77,17 @@ public class ApplicationService {
             }
         }
         return result;
+    }
+
+    public Company findCompanyById(int id){
+        
+        for (Company company : companies){
+
+            if( company.getId() == id){
+                return company;
+            }
+        }
+        return null;
     }
 
     public JobApplication findApplication(String roletitel){
