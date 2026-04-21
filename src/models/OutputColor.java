5c603495567a31f045355af892727287e3e38450
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-04-21 13:32:45 +0200

    Created enum for color codes

---
+++ src/models/OutputColor.java
@@ -0,0 +1,26 @@
+package src.models;
+
+enum OutputColor {
+    
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m"),
+    PURPLE("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m");
+
+    private final String code;
+
+    private OutputColor(String code) {
+        this.code = code;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+     
+
+    
+}
