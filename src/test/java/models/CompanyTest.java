d1afdbe070c1b44ba05422d9fd8bad4db9cff9a5
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-04-26 14:28:58 +0200

    Test invalid id

--- src/test/java/models/CompanyTest.java
+++ src/test/java/models/CompanyTest.java
@@ -1,5 +1,7 @@
 package models;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.net.URI;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,16 @@ public class CompanyTest {
     void CompanyConstructorAcceptsValidParameters(){
         try{
             assertNotNull( new Company(0,"IBM", "description", new URI("http://ibm.com"), 0) );
+        }catch(Exception e){
+            System.out.println("Caused by URI class: "+ e.getMessage());
+        }
+
+    }
+
+    @Test
+    void ConstructorRejectsInvalidId(){
+        try{
+            assertThrows( IllegalArgumentException.class , ()-> new Company(-1 ,"IBM", "description", new URI("http://ibm.com"), 0));
         }catch(Exception e){
             System.out.println("Caused by URI class: "+ e.getMessage());
         }
