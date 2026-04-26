83d1454708f9e6f84586e2f677721e955a72d406
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-04-26 14:30:06 +0200

    Test constructor with invalid name

--- src/test/java/models/CompanyTest.java
+++ src/test/java/models/CompanyTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.net.URI;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CompanyTest {
 
@@ -21,6 +23,17 @@ public class CompanyTest {
     void ConstructorRejectsInvalidId(){
         try{
             assertThrows( IllegalArgumentException.class , ()-> new Company(-1 ,"IBM", "description", new URI("http://ibm.com"), 0));
+        }catch(Exception e){
+            System.out.println("Caused by URI class: "+ e.getMessage());
+        }
+
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = {"", "  "})
+    void ConstructorRejectsInvalidName(String name){
+        try{
+            assertThrows( IllegalArgumentException.class , ()-> new Company(0  , name, "description", new URI("http://ibm.com"), 0));
         }catch(Exception e){
             System.out.println("Caused by URI class: "+ e.getMessage());
         }
