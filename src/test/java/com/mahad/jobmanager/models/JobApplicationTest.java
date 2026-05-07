3a7e97450c0fd3f78cc07c46824da2d7ad7a559b
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-07 14:42:42 +0200

    update test

--- src/test/java/com/mahad/jobmanager/models/JobApplicationTest.java
+++ src/test/java/com/mahad/jobmanager/models/JobApplicationTest.java
@@ -7,9 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
-
+@SpringBootTest
 public class JobApplicationTest {
 
     @Test
