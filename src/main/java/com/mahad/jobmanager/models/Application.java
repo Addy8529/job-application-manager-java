6fad838a5e43005a0a8d23e1239913dcacf828e4
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-07 13:12:40 +0200

    refractored

--- src/main/java/com/mahad/jobmanager/models/Application.java
+++ src/main/java/com/mahad/jobmanager/models/Application.java
@@ -1,14 +1,7 @@
 package com.mahad.jobmanager.models;
 
-import java.time.LocalDate;
 
 record Application(
     int id,
-    int companyId,
-    String roleTitle,
-    JobType jobType,
-    ApplicationStatus status,
-    LocalDate dateApplied,
-    LocalDate followUpDate,
-    String notes
+    String title
 ) {}
