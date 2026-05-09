784128026f34da33b2d4a4d76c66fd762ff5a00c
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-09 14:55:01 +0200

    update

--- src/main/java/com/mahad/jobmanager/models/Application.java
+++ src/main/java/com/mahad/jobmanager/models/Application.java
@@ -1,12 +1,14 @@
 package com.mahad.jobmanager.models;
 
-import org.springframework.data.annotation.Id;
 
 import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public record Application(
-    @Id Integer id,
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) Integer id,
     @NonNull String title
 ) {}
