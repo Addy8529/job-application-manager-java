a3c8eb1b0d2cb25b46bdbde98a24b58474797586
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-09 15:38:14 +0200

    test get endpoint

--- src/test/java/com/mahad/jobmanager/controller/ApplicationControllerTest.java
+++ src/test/java/com/mahad/jobmanager/controller/ApplicationControllerTest.java
@@ -1,10 +1,13 @@
 package com.mahad.jobmanager.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -13,6 +16,23 @@ public class ApplicationControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Test
+    void shouldReturnValidApplication() throws Exception{
+
+        mvc.perform(get("/app/1"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.title").value("SE"));
+    }
+
+    @Test
+    void shouldNotReturnApplicationIfItDoesNotExist() throws Exception{
+        mvc.perform(get("/app/1000"))
+        .andExpect(status().isNotFound());
+    }
+
+    
 
 
 
