15559c9745adb0cac6b486afa99f435213e6daf1
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-09 14:38:05 +0200

    update

--- src/main/java/com/mahad/jobmanager/models/Application.java
+++ src/main/java/com/mahad/jobmanager/models/Application.java
@@ -3,7 +3,9 @@ package com.mahad.jobmanager.models;
 import org.springframework.data.annotation.Id;
 
 import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Entity;
 
+@Entity
 public record Application(
     @Id Integer id,
     @NonNull String title
