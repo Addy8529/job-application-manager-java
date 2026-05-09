8e777371e64a049d7a883f13145e95b26f12ac10
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-09 15:33:29 +0200

    removed jpa

--- src/main/java/com/mahad/jobmanager/models/Application.java
+++ src/main/java/com/mahad/jobmanager/models/Application.java
@@ -1,14 +1,11 @@
 package com.mahad.jobmanager.models;
 
 
+import org.springframework.data.annotation.Id;
 import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity
+
 public record Application(
-    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) Integer id,
+    @Id Integer id,
     @NonNull String title
 ) {}
