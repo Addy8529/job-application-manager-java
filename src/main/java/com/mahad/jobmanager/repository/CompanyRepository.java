f4162628d442eb072f7809d7cc6c57caf50fcd6a
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-14 12:47:40 +0200

    added import

--- src/main/java/com/mahad/jobmanager/repository/CompanyRepository.java
+++ src/main/java/com/mahad/jobmanager/repository/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.mahad.jobmanager.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.mahad.jobmanager.models.NewCompany;
+
 
 public interface CompanyRepository extends CrudRepository<NewCompany, Integer> {
 
