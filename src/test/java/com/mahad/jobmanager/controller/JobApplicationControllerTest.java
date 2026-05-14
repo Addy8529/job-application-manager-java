af3f54d6a9369c81dada5382b8f37289c31552fe
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-14 12:48:52 +0200

    optimized imports

--- src/test/java/com/mahad/jobmanager/controller/JobApplicationControllerTest.java
+++ src/test/java/com/mahad/jobmanager/controller/JobApplicationControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.mahad.jobmanager.repository.NewCompany;
+import com.mahad.jobmanager.models.NewCompany;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
