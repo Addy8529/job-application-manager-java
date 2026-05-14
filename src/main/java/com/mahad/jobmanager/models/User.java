776290d3029a98ecb85d025f4025c6260fa8b6b3
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-14 14:32:16 +0200

    created user model class

---
+++ src/main/java/com/mahad/jobmanager/models/User.java
@@ -0,0 +1,57 @@
+package com.mahad.jobmanager.models;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NonNull
+    private String name;
+    @NonNull
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(String name, String password) {
+        this.id = null;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+
+}
