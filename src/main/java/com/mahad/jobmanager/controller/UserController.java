6d26f0902632c385053eca1853d0d5bf5a6db104
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-14 15:00:44 +0200

    added post mapping

--- src/main/java/com/mahad/jobmanager/controller/UserController.java
+++ src/main/java/com/mahad/jobmanager/controller/UserController.java
@@ -1,18 +1,31 @@
 package com.mahad.jobmanager.controller;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mahad.jobmanager.repository.UserRepository;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @PostMapping
-    private void createUser(){
+    @Autowired
+    private UserRepository repository;
 
+    @SuppressWarnings("null")
+    @PostMapping
+    private ResponseEntity<Void> createUser(){
+
+        return ResponseEntity.created(URI.create("/user/0")).build();
     }
+
+    
 
 
 }
