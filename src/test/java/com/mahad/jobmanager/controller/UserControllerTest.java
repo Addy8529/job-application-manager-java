89c4bacc0ba7b022e6f51c95b6942f1dfba48c8c
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-14 15:00:08 +0200

    test for post

--- src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
+++ src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
@@ -1,5 +1,11 @@
 package com.mahad.jobmanager.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +18,15 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Test
+    void shouldHavePostMapping() throws Exception{
+
+        mvc.perform(post("/user"))
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("location"));
+    }
+
     
+
+
 }
