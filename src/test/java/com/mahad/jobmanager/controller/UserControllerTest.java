cba61f3925adb9950961e8e02c4f178be25ba442
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-16 09:09:41 +0200

    updated tests

--- src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
+++ src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.mahad.jobmanager.models.User;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,13 +23,22 @@ public class UserControllerTest {
 
     @Test
     void shouldHavePostMapping() throws Exception{
-
-        mvc.perform(post("/user"))
+        String user = """
+                {
+        "name": "tester",
+        "password": "pass123"
+                }
+                """;
+        mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(user))
         .andExpect(status().isCreated())
         .andExpect(header().exists("location"));
     }
 
-    
+    @Test
+    void shouldActuallyCreateUser() throws Exception{
+        mvc.perform(get("/user/1"))
+        .andExpect(status().isOk());
+    }
 
 
 }
