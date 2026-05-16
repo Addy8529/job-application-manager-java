6ddc736071a4b35f0077b4562541f8e5dfb8761e
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-16 14:39:46 +0200

    added delete endpoint

--- src/main/java/com/mahad/jobmanager/controller/UserController.java
+++ src/main/java/com/mahad/jobmanager/controller/UserController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,23 @@ public class UserController {
         return ResponseEntity.ok(user.get());
         }
 
+
+    @PutMapping("/{id}")
+    private ResponseEntity<Void> updateUser(@PathVariable("id") Integer id, @RequestBody User user){
+
+        User newUser = new User(id, user.getName(), user.getPassword());
+
+        repository.save(newUser);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteUser(@PathVariable("id") Integer id){
+
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
+    }
 
 
 
