eac9e72dd4396271943bfe8428e347a8711fdf70
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-16 14:55:59 +0200

    updated tests

--- src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
+++ src/test/java/com/mahad/jobmanager/controller/UserControllerTest.java
@@ -1,8 +1,12 @@
 package com.mahad.jobmanager.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.mahad.jobmanager.models.User;
 
@@ -36,8 +41,63 @@ public class UserControllerTest {
 
     @Test
     void shouldActuallyCreateUser() throws Exception{
+        String name = "Tester";
+        String password = "pass123";
+        String url = createUserAndGetLocation( name, password);
+        mvc.perform(get(url))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id").exists())
+        .andExpect(jsonPath("$.name").value(name))
+        .andExpect(jsonPath("$.password").value(password));
+    }
+
+    @Test
+    void shouldUpdateExistingUser() throws Exception{
+
+        mvc.perform(put("/user/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("""
+                {
+                "id":null,
+                "name": "tester",
+                "password": "pass456"
+                }
+                """)).andExpect(status().isNoContent());
+
         mvc.perform(get("/user/1"))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.password").value("pass456"));
+    }
+
+    @Test
+    void shouldDeleteExistingUser() throws Exception{
+        String url = createUserAndGetLocation("deleteTester", "delete123");
+
+        mvc.perform(get(url)).andExpect(status().isOk());
+        mvc.perform(delete(url)).andExpect(status().isNoContent());
+        mvc.perform(get(url)).andExpect(status().isNotFound()); 
+    }
+
+   private String createUserAndGetLocation( String name, String password)throws Exception{
+
+        return mvc.perform(post("/user")
+        .contentType(MediaType.APPLICATION_JSON).content(UserJson(name, password)))
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("location"))
+        .andReturn().getResponse().getHeader("location");
+
+   }
+
+
+   private String UserJson(String name, String password) {
+        return """
+                {
+                    "id": null,
+                    "name": "%s",
+                    "password": "%s"
+                }
+                """.formatted(name, password);
     }
 
 
