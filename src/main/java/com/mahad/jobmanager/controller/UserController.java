1009766dca3d12c42e45dfe68d6f6758334a3980
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-16 09:09:23 +0200

    added get mapping

--- src/main/java/com/mahad/jobmanager/controller/UserController.java
+++ src/main/java/com/mahad/jobmanager/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.mahad.jobmanager.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mahad.jobmanager.models.User;
 import com.mahad.jobmanager.repository.UserRepository;
+
+import io.micrometer.common.lang.NonNull;
 
 @RestController
 @RequestMapping("/user")
@@ -20,12 +26,24 @@ public class UserController {
 
     @SuppressWarnings("null")
     @PostMapping
-    private ResponseEntity<Void> createUser(){
-
-        return ResponseEntity.created(URI.create("/user/0")).build();
+    private ResponseEntity<Void> createUser(@RequestBody User user){
+        User createdUser = repository.save(user);
+        return ResponseEntity.created(URI.create("/user/"+createdUser.getId())).build();
     }
 
-    
+    @GetMapping("/{id}")
+    private ResponseEntity<User> getUser( @PathVariable("id") Integer id){
+
+        Optional<User> user = repository.findById(id);
+
+        if (user.isEmpty()){
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user.get());
+        }
+
+
+
 
 
 }
