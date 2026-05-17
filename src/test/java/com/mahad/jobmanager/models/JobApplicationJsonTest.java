746735aef463bd802ebac0c633b1c90352ec4f7a
Addy8529 <Mohammad.ahad.321@gmail.com>
2026-05-17 15:26:54 +0200

    implemented test cases

--- src/test/java/com/mahad/jobmanager/models/JobApplicationJsonTest.java
+++ src/test/java/com/mahad/jobmanager/models/JobApplicationJsonTest.java
@@ -3,6 +3,7 @@ package com.mahad.jobmanager.models;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,25 @@ import org.springframework.boot.test.json.JacksonTester;
 public class JobApplicationJsonTest {
 
     @Autowired
-    private JacksonTester<Application> json;
+    private JacksonTester<JobApplication> json;
 
     @Test
     void shouldCreateValidJson() throws IOException{
-        //Application application =  new Application(0,"Software Developer");
+        JobApplication application =  new JobApplication(
+            0,
+            0,
+            "Software Developer",
+            JobType.FULL_TIME,
+            ApplicationStatus.APPLIED,
+            LocalDate.of(2026, 5, 7),
+            LocalDate.of(2026, 5, 14),
+            "No notes."
+        );
 
-        //assertThat(json.write(application)).isStrictlyEqualToJson("app.json");
+        assertThat(json.write(application)).isStrictlyEqualToJson("expectedApplication.json");
+        assertThat(json.write(application)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(application)).extractingJsonPathStringValue("@.roleTitle")
+                .isEqualTo("Software Developer");
 
 
     }
